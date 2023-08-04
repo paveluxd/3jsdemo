b3ed2f52ebfac14d766198ac782e03019faae059
paveluxd <44015330+paveluxd@users.noreply.github.com>
2023-08-04 16:44:18 +0200

    1

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/iREFo0fi25j4ICAX/scene.splinecode');
