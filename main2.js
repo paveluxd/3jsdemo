d694a4b10b422d4342bf6a38d96eba25eb3adcf1
paveluxd <44015330+paveluxd@users.noreply.github.com>
2023-09-01 17:13:09 +0200

    1

--- main2.js
+++ main2.js
@@ -3,3 +3,7 @@ import { Application } from '@splinetool/runtime';
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/1bFEnkogC-DIMOt6/scene.splinecode');
+
+const canvas2 = document.getElementById('canvas3d-2');
+const app2 = new Application(canvas2);
+app2.load('https://prod.spline.design/vcESI8Dq8Ci4-6tS/scene.splinecode');
